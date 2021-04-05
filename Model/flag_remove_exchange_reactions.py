a3db5f3328b38a4a16461f374da2c7857e0d2959
Marco Corrao <mc5517@ic.ac.uk>
2021-04-05 18:59:47 +0100

    fixed model wo exchange reaction

--- Model/flag_remove_exchange_reactions.py
+++ Model/flag_remove_exchange_reactions.py
@@ -11,7 +11,7 @@ import copy
 #Model where the exchange reactions are flagged
 m_flag=cobra.io.load_json_model('Escherichia_coli_iCH360.json')
 #Model where the exchange reactions are removed
-m_remove=copy.deepcopy(m_flag)
+m_remove=cobra.io.load_json_model('Escherichia_coli_iCH360.json')
 #%% Flag Exchange reactions
 for r in m_flag.reactions:
     if r.id[0:3]=='EX_':
@@ -20,13 +20,15 @@ for r in m_flag.reactions:
         r.notes['exchange_reaction']='false'
 
 #Save the updated model
-cobra.io.save_json_model(m_flag, 'model_w_exchange_flagged/Escherichia_coli_iCH360_exchange_flagged.json')
-cobra.io.write_sbml_model(m_flag, 'model_w_exchange_flagged/Escherichia_coli_iCH360_exchange_flagged.xml')
+#cobra.io.save_json_model(m_flag, 'model_w_exchange_flagged/Escherichia_coli_iCH360_exchange_flagged.json')
+#cobra.io.write_sbml_model(m_flag, 'model_w_exchange_flagged/Escherichia_coli_iCH360_exchange_flagged.xml')
 
 #%% Remove Exchange reacions
+to_delete=[]
 for r in m_remove.reactions:
     if r.id[0:3]=='EX_':
-        m_remove.remove_reactions([r])
+        to_delete.append(r)
+m_remove.remove_reactions(to_delete)       
 
-cobra.io.save_json_model(m_flag, 'model_wo_exchange/Escherichia_coli_iCH360_wo_exchange.json')
-cobra.io.write_sbml_model(m_flag, 'model_wo_exchange/Escherichia_coli_iCH360_wo_exchange.xml')
+cobra.io.save_json_model(m_remove, 'model_wo_exchange/Escherichia_coli_iCH360_wo_exchange.json')
+#cobra.io.write_sbml_model(m_remove, 'model_wo_exchange/Escherichia_coli_iCH360_wo_exchange.xml')
